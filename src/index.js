cef06c6307e5999d4977b25cb5654aa68bca4dc0
markchristopherbishop <mark.christopher.bishop@gmail.com>
2018-09-05 21:54:14 +0200

    add proxy option

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const express = require('express');
 const runCommand = require('./runCommand');
 const getPort = require('./getPort');
 const createInjectionMiddleware = require('./injectionMiddleware');
+const proxy = require('express-http-proxy');
 
 program
-  .version('0.1.0');
+  .version('0.0.1');
 
 program
   .command('run [command]')
   .description('run a test command against a target host or proxy')
-  .option('-h, --host [host]', 'The directory to host the application for testing', './dist')
+  .option('-h, --host [host]', 'The directory to host for application testing')
+  .option('-p, --proxy [proxy]', 'A website to proxy for application testing')
   .option('-i, --inject [inject]', 'Script to inject')
   .action(async (command, options) => {
     const host = 'localhost';
@@ -19,12 +21,25 @@ program
     process.env.TEST_PORT = port;
     const app = express();
     
+    if (!options.host && !options.proxy) {
+      throw new Error('You must supply either -h (--host) or -p (--proxy) options with the run command');
+    } else if (options.host && options.proxy) {
+      throw new Error('You cannot supply both -h (--host) and -p (--proxy) options with the run command. It must be one or the other.');
+    }
+    
     if (options.inject) {
       app.use(createInjectionMiddleware(options.inject));
     }
-    app.use(express.static(options.host));
-    app.listen(port, async () => {
+    
+    if (options.host) {
       console.log(`Hosting directory ${options.host} on http://${host}:${port}`);
+      app.use(express.static(options.host));
+    } else if (options.proxy) {
+      console.log(`Proxing ${options.proxy} for test on http://${host}:${port}`);
+      app.use('/', proxy(options.proxy))
+    }
+    
+    app.listen(port, async () => {
       console.log('Testing started');
       const { code } = await runCommand(command);
       console.log('Testing complete');
