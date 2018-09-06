58d13ea2502944cc95b60a593dcc3088892ae001
markchristopherbishop <mark.christopher.bishop@gmail.com>
2018-09-06 08:47:35 +0200

    add readme.md

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 #!/usr/bin/env node
 const program = require('commander');
 const express = require('express'); 
+const proxy = require('express-http-proxy');
 const runCommand = require('./runCommand');
 const getPort = require('./getPort');
 const createInjectionMiddleware = require('./injectionMiddleware');
-const proxy = require('express-http-proxy');
+const package = require('../package');
 
 program
-  .version('0.0.1');
+  .version(package.version);
 
 program
   .command('run [command]')
   .description('run a test command against a target host or proxy')
   .option('-h, --host [host]', 'The directory to host for application testing')
-  .option('-p, --proxy [proxy]', 'A website to proxy for application testing')
+  .option('-x, --proxy [proxy]', 'A website to proxy for application testing')
   .option('-i, --inject [inject]', 'Script to inject')
+  .option('-p, --port [port]', 'The port to use for testing')
   .action(async (command, options) => {
     const host = 'localhost';
-    const port = await getPort();
+    const port = options.port || await getPort();
     process.env.TEST_PORT = port;
     const app = express();
     
     if (!options.host && !options.proxy) {
-      throw new Error('You must supply either -h (--host) or -p (--proxy) options with the run command');
+      throw new Error('You must supply either -h (--host) or -x (--proxy) options with the run command');
     } else if (options.host && options.proxy) {
-      throw new Error('You cannot supply both -h (--host) and -p (--proxy) options with the run command. It must be one or the other.');
+      throw new Error('You cannot supply both -h (--host) and -x (--proxy) options with the run command. It must be one or the other.');
     }
     
     if (options.inject) {
